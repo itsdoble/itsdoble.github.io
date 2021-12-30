7286163703dd2b19fddc6a89bfcaa3a4c6cb2ce5
itsdoble <doblegangerlol@gmail.com>
2021-12-30 11:13:36 +0530

    removed some unneeded files

--- gh.ps1
+++
@@ -1,4 +0,0 @@
-Remove-Item "Y:\Documents\My work\My_Page\itsdoble.github.io\*" -Force
-Copy-Item -Path "Y:\Documents\My work\Projects\itsdoble\build\*" -Destination "Y:\Documents\My work\My_Page\itsdoble.github.io\";
-
-
